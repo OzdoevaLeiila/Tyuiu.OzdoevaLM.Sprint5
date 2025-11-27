aec90a9e7de4e12e68bd18a87e17fd8ec5ec3df6
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-11-27 23:50:21 +0500

    Task5

---
+++ Tyuiu.OzdoevaLM.Sprint5.Task5.V11.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.OzdoevaLM.Sprint5.Task5.V11.Lib
+{
+    public class DataService : ISprint5Task5V11
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double product = 1;
+
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    string[] numbers = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var numberStr in numbers)
+                    {
+
+                        if (int.TryParse(numberStr, out int number) && number % 2 != 0)
+                        {
+                            product *= number;
+
+                        }
+                    }
+                }
+            }
+
+            return Math.Round(product, 3);
+        }
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint5.Task5.V11/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.IO;
+using Tyuiu.OzdoevaLM.Sprint5.Task5.V11.Lib;
+namespace Tyuiu.OzdoevaLM.Sprint5.Task5.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5| Выполнила: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Оздоева Л.М. | ИИПб-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 3, результат сохранить в *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "DataSprint5", "InPutDataFileTask5V11.txt");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
