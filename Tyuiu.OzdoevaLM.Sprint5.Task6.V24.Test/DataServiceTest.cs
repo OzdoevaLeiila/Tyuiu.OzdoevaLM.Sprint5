aebca47d19ee74dca9a453298d15112f94638996
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-11-28 00:26:25 +0500

    Task6

---
+++ Tyuiu.OzdoevaLM.Sprint5.Task6.V24.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.OzdoevaLM.Sprint5.Task6.V24.Lib;
+namespace Tyuiu.OzdoevaLM.Sprint5.Task6.V24.Test;
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckFileExistence()
+    {
+        var path = @"C:\DataSprint5\InPutDataFileTask6V24.txt";
+        Assert.IsTrue(File.Exists(path));
+    }
+
+    [TestMethod]
+    public void CheckLoadFromDataFile()
+    {
+        var ds = new DataService();
+        Assert.AreEqual(2, ds.LoadFromDataFile(@"C:\DataSprint5\InPutDataFileTask6V24.txt"));
+    }
+}
---
+++ Tyuiu.OzdoevaLM.Sprint5.Task6.V24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Tyuiu.OzdoevaLM.Sprint5.Task6.V24.Lib;
+
+namespace Tyuiu.OzdoevaLM.Sprint5.Task6.V24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5| Выполнила: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: обработка текстовых файлов                                        *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Выполнила: Оздоева Л.М. | ИИПб-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти количество пятизначных чисел в заданной строке.                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V24.txt";
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Результат: " + res);
+            Console.ReadLine();
+        }
+    }
+}
