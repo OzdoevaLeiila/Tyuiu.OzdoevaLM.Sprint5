6b086e44a78b241aa7cbd5354ef88993048091c7
OzdoevaLeiila <tyuiu.ozdoevalm@outlook.com>
2025-11-27 19:19:12 +0500

    Task1

---
+++ Tyuiu.OzdoevaLM.Sprint5.Task1.V13/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.OzdoevaLM.Sprint5.Task1.V13.Lib;
+namespace Tyuiu.OzdoevaLM.Sprint5.Task1.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Оздоева Л.М. | ИИПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнила: Оздоева Л.М. | ИИПб-25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана функция,(произвести табулирование) f(x) на заданном диапазоне      *");
+            Console.WriteLine("* [-5; 5] с шагом 1. Произвести проверку деления на ноль. При делении на  *");
+            Console.WriteLine("* ноль вернуть значение 0. результат сохранить в текстовый файл           *");
+            Console.WriteLine("* OutPutFileTask1.txt и вывести на консоль в таблицу.                     *");
+            Console.WriteLine("* Значения округлить до двух знаков после запятой.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int start = - 15;
+            int stop = 15;
+
+            Console.WriteLine("Начало диапазона = " + start);
+            Console.WriteLine("Конец диапазона = " + stop);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(start,stop);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadLine();
+        }
+    }
+}
